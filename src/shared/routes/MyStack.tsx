8fe04ac5edbda2b6e0325d30e1eaf5c4e54a4f48
jaine-valari <jaine@valarisolutions.com>
2024-04-24 02:38:05 +0200

    feat: create home container and pass movies as prop

--- src/shared/routes/MyStack.tsx
+++ src/shared/routes/MyStack.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from './home/Home';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import HomeContainer from './home/HomeContainer';
 
 export type RootStackParamList = {
   Home: undefined; // undefined because you aren't passing any params to the home screen
@@ -16,7 +16,7 @@ const MyStack = () => {
     <NavigationContainer>
       <QueryClientProvider client={queryClient}>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Home" component={HomeContainer} />
         </Stack.Navigator>
       </QueryClientProvider>
     </NavigationContainer>
