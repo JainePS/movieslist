0e72742148e0ef0dfea51d4d1faca33e181b7d32
jaine-valari <jaine@valarisolutions.com>
2024-04-24 02:05:19 +0200

    feat: create movies types and fetch movies by genre

---
+++ src/shared/utils/moviesCategories.ts
@@ -0,0 +1,12 @@
+export enum MovieCategory {
+  ACTION_ADVENTURE = 'action-adventure',
+  ANIMATION = 'animation',
+  CLASSIC = 'classic',
+  COMEDY = 'comedy',
+  DRAMA = 'drama',
+  HORROR = 'horror',
+  FAMILY = 'family',
+  MYSTERY = 'mystery',
+  SCIFI_FANTASY = 'scifi-fantasy',
+  WESTERN = 'western',
+}
