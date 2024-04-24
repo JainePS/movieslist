a8e866dba3759646489b5a20dc72b2f9b02785eb
jaine-valari <jaine@valarisolutions.com>
2024-04-24 02:01:01 +0200

    fix: remove unused imports from app and create the stack of routes

--- src/shared/routes/MyStack.tsx
+++ src/shared/routes/MyStack.tsx
@@ -2,19 +2,23 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './home/Home';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined; // undefined because you aren't passing any params to the home screen
+};
+const queryClient = new QueryClient();
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const MyStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{title: 'Welcome'}}
-        />
-      </Stack.Navigator>
+      <QueryClientProvider client={queryClient}>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </QueryClientProvider>
     </NavigationContainer>
   );
 };
