c41f05d453f79f7e4914cf56a5d52271cb87c932
jaine-valari <jaine@valarisolutions.com>
2024-04-26 01:17:38 +0200

    chore: remove unused dependency

---
+++ src/modules/movie/components/headers/GenresHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SelectorHeader from '../../../../shared/components/molecules/SelectorHeader';
+
+const GenresHeader = () => {
+  return (
+    <SelectorHeader elements={[]} onPress={_id => {}} selectedElementID="123" />
+  );
+};
+
+export default GenresHeader;
