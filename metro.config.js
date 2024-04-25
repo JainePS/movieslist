f1b244188826ba8d7172d708f60d4e9ea69efdc3
jaine-valari <jaine@valarisolutions.com>
2024-04-25 11:46:35 +0200

    chore: add custom fonts

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,12 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/'],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
