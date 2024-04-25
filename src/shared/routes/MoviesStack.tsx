99580a556390fc1a74ffb81d91073807c2c71882
jaine-valari <jaine@valarisolutions.com>
2024-04-26 01:04:24 +0200

    chore: rename MoviesStack

--- src/shared/routes/MoviesStack.tsx
+++ src/shared/routes/MoviesStack.tsx
@@ -4,7 +4,6 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Home from './home';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import Header from '../components/templates/Header';
 
 export type RootStackParamList = {
   Home: undefined; // undefined because you aren't passing any params to the home screen
@@ -13,17 +12,13 @@ const queryClient = new QueryClient();
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-const MyStack = () => {
+const MoviesStack = () => {
   return (
     <NavigationContainer>
       <QueryClientProvider client={queryClient}>
         <SafeAreaProvider>
           <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen
-              name="Home"
-              component={Home}
-              options={{header: Header}}
-            />
+            <Stack.Screen name="Home" component={Home} />
           </Stack.Navigator>
         </SafeAreaProvider>
       </QueryClientProvider>
@@ -31,4 +26,4 @@ const MyStack = () => {
   );
 };
 
-export default MyStack;
+export default MoviesStack;
