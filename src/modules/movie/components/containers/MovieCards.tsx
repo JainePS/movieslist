390d9bd1d9676b6cfc75ab15664bcce41aeddd5f
jaine-valari <jaine@valarisolutions.com>
2024-04-27 16:27:57 +0200

    fix: card layout

--- src/modules/movie/components/containers/MovieCards.tsx
+++ src/modules/movie/components/containers/MovieCards.tsx
@@ -12,34 +12,40 @@ import {useMoviesContext} from '../../context/MoviesContext';
 const MovieCards = () => {
   const {movies, moviesError, IsMoviesLoading} = useMoviesContext();
 
-  console.log(moviesError);
-  
-
   if (IsMoviesLoading) {
     return <ActivityIndicator />;
   }
 
   if (moviesError) {
-    <View>
-      <Text>There was an error loading the movies of this genre</Text>
-    </View>;
+    return (
+      <View style={styles.errorContainer}>
+        <Text style={styles.textError}>
+          There was an error loading the movies of this genre.
+        </Text>
+      </View>
+    );
   }
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={movies}
-        renderItem={({item}) => <Card movie={item} />}
-        keyExtractor={item => `${item.id}`}
-        style={styles.container}
-      />
-    </View>
+    <FlatList
+      data={movies}
+      renderItem={({item}) => <Card movie={item} />}
+      keyExtractor={item => `${item.id}`}
+      style={styles.container}
+    />
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     columnGap: 4,
+  },
+  errorContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '50%',
+    padding: 5,
   },
   flexRow: {
     display: 'flex',
@@ -56,6 +62,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     justifyContent: 'space-between',
     borderRadius: 8,
+  },
+  textError: {
+    fontSize: 18,
+    color: 'red',
   },
 });
 
